3122724b805163833fc5ab3c6740eff103ecae37
SmitTandel123 <134622676+SmitTandel123@users.noreply.github.com>
2023-07-30 22:44:12 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css"
+const App = ()=>{
+  return(
+    <div>
+      <h2>Welcome to My portfolio</h2>
+    </div>
+  )
+}
+export default App
